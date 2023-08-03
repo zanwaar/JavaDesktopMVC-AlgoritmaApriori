418ecd3bbae4cffa31996ec75ab94366b1dd29a3
zanwaar <zanwaarsamjid@gmail.com>
2023-08-03 08:11:49 +0800

    Update Database

--- DB/db_tokosembakoberkah.sql
+++ DB/db_tokosembakoberkah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2023 at 04:31 AM
+-- Generation Time: Aug 03, 2023 at 02:10 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -42,11 +42,16 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id`, `kode`, `nama`, `kategori`, `satuan`, `stok`, `hargaSatuan`) VALUES
-(44, 'BS', 'beras', 'sembako', 'karung', 40, 10000),
-(46, 'S1', 'susu', 'sembako', 'kardus', 995, 250000),
-(47, 'TK1', 'Telur', 'Telur', 'rak', 300, 240000),
-(48, 'MTG', 'Mentega bkueband', 'Mentega', 'Kardus', 10055, 20000),
-(49, 'GlA-12', 'Gula', 'Gula', 'Karung', 1060, 20000);
+(51, 'J', 'Sagu 500g', 'Sagu', 'gram', 1, 5000),
+(52, 'I', 'Kembang Tahu 250g', 'Kembang Tahu', 'Gram', 14, 3000),
+(53, 'H', 'Roti Tawar Sari Roti', 'Roti', 'bungkus', 40, 12000),
+(54, 'G', 'Kopi Kapal Api Bubuk 200g', 'Kopi', 'gram', 23, 18000),
+(55, 'F', 'Teh Celup Sariwangi 25 bungkus', 'Teh', 'bungkus', 75, 15000),
+(56, 'E', 'Sarden Kaleng 425g', 'Sarden', 'gram', 10, 9000),
+(57, 'D', 'Sambal ABC Botol 135g', 'Sambal', 'gram', 19, 7000),
+(58, 'C', 'Kecap Manis ABC 620ml', 'Kecap Manis', 'ml', 47, 10000),
+(59, 'B', 'Minyak Goreng Bimoli 1 liter', 'Minyak Goreng', 'liter', 39, 20000),
+(60, 'A', 'Beras Medium 5kg', 'Beras', 'kg', 91, 50000);
 
 -- --------------------------------------------------------
 
@@ -68,30 +73,70 @@ CREATE TABLE `detail_transaksi` (
 --
 
 INSERT INTO `detail_transaksi` (`id`, `id_transaksi`, `nama_barang`, `kategori`, `satuan`, `jumlah`) VALUES
-(54, 32, 'Gula', 'Gula', 1000, 20000000),
-(55, 33, 'Mentega bkueband', 'Mentega', 44, 880000),
-(56, 34, 'susu', 'sembako', 6, 1440000),
-(57, 35, 'beras', 'sembako', 5, 50000),
-(58, 36, 'Gula', 'Gula', 7, 140000),
-(59, 37, 'susu', 'sembako', 2, 500000),
-(60, 38, 'Gula', 'Gula', 6, 120000),
-(61, 39, 'Gula', 'Gula', 7, 140000),
-(62, 40, 'Gula', 'Gula', 6, 120000),
-(63, 41, 'Gula', 'Gula', 5, 100000),
-(64, 42, 'beras', 'sembako', 2, 20000),
-(65, 43, 'Mentega bkueband', 'Mentega', 1, 20000),
-(66, 44, 'Gula', 'Gula', 4, 80000),
-(67, 44, 'susu', 'sembako', 6, 1500000),
-(68, 45, 'beras', 'sembako', 3, 30000),
-(69, 46, 'Gula', 'Gula', 4, 80000),
-(70, 46, 'susu', 'sembako', 6, 1500000),
-(71, 47, 'Gula', 'Gula', 7, 140000),
-(72, 48, 'susu', 'sembako', 1000, 250000000),
-(73, 49, 'susu', 'sembako', 5, 1250000),
-(74, 49, 'Gula', 'Gula', 3, 60000),
-(75, 50, 'Telur', 'Telur', 300, 72000000),
-(76, 51, 'Telur', 'Telur', 50, 12000000),
-(77, 52, 'Telur', 'Telur', 50, 12000000);
+(80, 55, 'Beras Medium 5kg', 'Beras', 2, 100000),
+(81, 55, 'Sambal ABC Botol 135g', 'Sambal', 1, 7000),
+(82, 55, 'Teh Celup Sariwangi 25 bungkus', 'Teh', 2, 30000),
+(83, 56, 'Minyak Goreng Bimoli 1 liter', 'Minyak Goreng', 2, 40000),
+(84, 56, 'Kecap Manis ABC 620ml', 'Kecap Manis', 3, 30000),
+(85, 56, 'Kopi Kapal Api Bubuk 200g', 'Kopi', 1, 18000),
+(86, 56, 'Roti Tawar Sari Roti', 'Roti', 1, 12000),
+(87, 57, 'Beras Medium 5kg', 'Beras', 1, 50000),
+(88, 57, 'Kecap Manis ABC 620ml', 'Kecap Manis', 2, 20000),
+(89, 57, 'Roti Tawar Sari Roti', 'Roti', 2, 24000),
+(90, 57, 'Kembang Tahu 250g', 'Kembang Tahu', 2, 6000),
+(91, 57, 'Sagu 500g', 'Sagu', 2, 10000),
+(92, 58, 'Kecap Manis ABC 620ml', 'Kecap Manis', 2, 20000),
+(93, 58, 'Teh Celup Sariwangi 25 bungkus', 'Teh', 2, 30000),
+(94, 58, 'Roti Tawar Sari Roti', 'Roti', 4, 48000),
+(95, 59, 'Minyak Goreng Bimoli 1 liter', 'Minyak Goreng', 4, 80000),
+(96, 59, 'Sambal ABC Botol 135g', 'Sambal', 1, 7000),
+(97, 60, 'Sambal ABC Botol 135g', 'Sambal', 4, 28000),
+(98, 60, 'Sarden Kaleng 425g', 'Sarden', 2, 18000),
+(99, 60, 'Kembang Tahu 250g', 'Kembang Tahu', 6, 18000),
+(100, 60, 'Sagu 500g', 'Sagu', 3, 15000),
+(101, 61, 'Kecap Manis ABC 620ml', 'Kecap Manis', 4, 40000),
+(102, 61, 'Roti Tawar Sari Roti', 'Roti', 4, 48000),
+(103, 62, 'Kopi Kapal Api Bubuk 200g', 'Kopi', 3, 54000),
+(104, 62, 'Kembang Tahu 250g', 'Kembang Tahu', 1, 3000),
+(105, 63, 'Teh Celup Sariwangi 25 bungkus', 'Teh', 4, 60000),
+(106, 63, 'Kembang Tahu 250g', 'Kembang Tahu', 5, 15000),
+(107, 63, 'Sagu 500g', 'Sagu', 2, 10000),
+(108, 64, 'Beras Medium 5kg', 'Beras', 3, 150000),
+(109, 64, 'Kecap Manis ABC 620ml', 'Kecap Manis', 3, 30000),
+(110, 65, 'Minyak Goreng Bimoli 1 liter', 'Minyak Goreng', 3, 60000),
+(111, 65, 'Sambal ABC Botol 135g', 'Sambal', 4, 28000),
+(112, 65, 'Sarden Kaleng 425g', 'Sarden', 2, 18000),
+(113, 65, 'Kembang Tahu 250g', 'Kembang Tahu', 6, 18000),
+(114, 65, 'Sagu 500g', 'Sagu', 7, 35000),
+(115, 66, 'Minyak Goreng Bimoli 1 liter', 'Minyak Goreng', 2, 40000),
+(116, 66, 'Kecap Manis ABC 620ml', 'Kecap Manis', 4, 40000),
+(117, 66, 'Kopi Kapal Api Bubuk 200g', 'Kopi', 5, 90000),
+(118, 66, 'Roti Tawar Sari Roti', 'Roti', 6, 72000),
+(119, 67, 'Sambal ABC Botol 135g', 'Sambal', 5, 35000),
+(120, 67, 'Roti Tawar Sari Roti', 'Roti', 2, 24000),
+(121, 67, 'Kembang Tahu 250g', 'Kembang Tahu', 4, 12000),
+(122, 67, 'Sagu 500g', 'Sagu', 2, 10000),
+(123, 68, 'Kecap Manis ABC 620ml', 'Kecap Manis', 3, 30000),
+(124, 68, 'Teh Celup Sariwangi 25 bungkus', 'Teh', 2, 30000),
+(125, 68, 'Roti Tawar Sari Roti', 'Roti', 2, 24000),
+(126, 69, 'Beras Medium 5kg', 'Beras', 2, 100000),
+(127, 69, 'Kembang Tahu 250g', 'Kembang Tahu', 5, 15000),
+(128, 69, 'Sagu 500g', 'Sagu', 6, 30000),
+(129, 70, 'Sambal ABC Botol 135g', 'Sambal', 6, 42000),
+(130, 70, 'Sarden Kaleng 425g', 'Sarden', 6, 54000),
+(131, 70, 'Kembang Tahu 250g', 'Kembang Tahu', 5, 15000),
+(132, 70, 'Sagu 500g', 'Sagu', 1, 5000),
+(133, 71, 'Kecap Manis ABC 620ml', 'Kecap Manis', 1, 10000),
+(134, 71, 'Teh Celup Sariwangi 25 bungkus', 'Teh', 5, 75000),
+(135, 71, 'Roti Tawar Sari Roti', 'Roti', 3, 36000),
+(136, 72, 'Kopi Kapal Api Bubuk 200g', 'Kopi', 2, 36000),
+(137, 72, 'Kembang Tahu 250g', 'Kembang Tahu', 1, 3000),
+(138, 73, 'Kopi Kapal Api Bubuk 200g', 'Kopi', 1, 18000),
+(139, 73, 'Kembang Tahu 250g', 'Kembang Tahu', 1, 3000),
+(140, 73, 'Sagu 500g', 'Sagu', 1, 5000),
+(141, 74, 'Beras Medium 5kg', 'Beras', 1, 50000),
+(142, 74, 'Kecap Manis ABC 620ml', 'Kecap Manis', 1, 10000),
+(143, 74, 'Roti Tawar Sari Roti', 'Roti', 1, 12000);
 
 -- --------------------------------------------------------
 
@@ -114,10 +159,7 @@ CREATE TABLE `entitas` (
 INSERT INTO `entitas` (`id`, `type`, `nama`, `alamat`, `no_telp`) VALUES
 (0, '-', '-', '-', '-'),
 (1, 'Supplier', 'alan jamaludin', 'jln mujair', '081239981831'),
-(2, 'pelanggan', 'iwan', 'batukel pasar binaya', '08122383747457'),
-(9, 'pelanggan', 'reno', 'alamat', 'p09087968'),
-(13, 'Supplier', 'reno', 'adfafd', '3465376'),
-(14, 'Supplier', 'tes', 'qwq', '12');
+(2, 'pelanggan', 'iwan', 'batukel pasar binaya', '08122383747457');
 
 -- --------------------------------------------------------
 
@@ -152,27 +194,26 @@ CREATE TABLE `transaksi` (
 --
 
 INSERT INTO `transaksi` (`id`, `status`, `idUser`, `idSp`, `invoice`, `subTotal`, `tanggal`) VALUES
-(32, 'Barang Masuk', 1, 0, 'INV-BM-2023070718584035154', 20000000, '2023-07-07'),
-(33, 'Barang Keluar', 1, 0, 'INV-BM-2023070719103442725', 880000, '2023-07-07'),
-(34, 'Barang Keluar', 1, 0, 'INV-BM-2023070719110819638', 1440000, '2023-07-07'),
-(35, 'Barang Keluar', 1, 0, 'INV-BM-2023070719114839147', 50000, '2023-07-07'),
-(36, 'Barang Keluar', 1, 0, 'INV-BM-2023070719120042105', 140000, '2023-07-07'),
-(37, 'Barang Keluar', 1, 0, 'INV-BM-2023070719121864598', 500000, '2023-07-07'),
-(38, 'Barang Masuk', 1, 0, 'INV-BM-2023070719124749962', 120000, '2023-07-07'),
-(39, 'Barang Keluar', 1, 0, 'INV-BM-2023070719125981832', 140000, '2023-07-07'),
-(40, 'Barang Keluar', 1, 0, 'INV-BM-2023070719131117824', 120000, '2023-07-07'),
-(41, 'Barang Keluar', 1, 0, 'INV-BM-2023070720224180833', 100000, '2023-07-07'),
-(42, 'Barang Keluar', 1, 0, 'INV-BM-2023070720334271925', 20000, '2023-07-07'),
-(43, 'Barang Keluar', 1, 0, 'INV-BM-2023070720340991714', 20000, '2023-07-07'),
-(44, 'Barang Keluar', 1, 0, 'INV-BM-2023070720345070648', 1580000, '2023-07-07'),
-(45, 'Barang Keluar', 1, 0, 'INV-BM-2023070720404132647', 30000, '2023-07-07'),
-(46, 'Barang Keluar', 1, 0, 'INV-BM-2023070720421032244', 1580000, '2023-07-07'),
-(47, 'Barang Keluar', 1, 0, 'INV-BM-2023070720423088210', 140000, '2023-07-07'),
-(48, 'Barang Masuk', 1, 0, 'INV-BM-2023070720430221656', 250000000, '2023-07-07'),
-(49, 'Barang Keluar', 1, 0, 'INV-BM-2023070720442891164', 1310000, '2023-07-07'),
-(50, 'Barang Masuk', 1, 0, 'INV-BM-2023070721000672214', 72000000, '2023-07-07'),
-(51, 'Barang Masuk', 1, 0, 'INV-BM-2023070721010353069', 12000000, '2023-07-07'),
-(52, 'Barang Keluar', 1, 0, 'INV-BM-2023070721012066267', 12000000, '2023-07-07');
+(55, 'Barang Keluar', 1, 0, 'INV-BM-2023072816284018784', 137000, '2023-07-28'),
+(56, 'Barang Keluar', 1, 0, 'INV-BM-2023072816292368182', 100000, '2023-07-28'),
+(57, 'Barang Keluar', 1, 0, 'INV-BM-2023072816310024584', 110000, '2023-07-28'),
+(58, 'Barang Keluar', 1, 0, 'INV-BM-2023072816313963799', 98000, '2023-07-28'),
+(59, 'Barang Keluar', 1, 0, 'INV-BM-2023072816320833023', 87000, '2023-07-28'),
+(60, 'Barang Keluar', 1, 0, 'INV-BM-2023072816325925123', 79000, '2023-07-28'),
+(61, 'Barang Keluar', 1, 0, 'INV-BM-2023072816331957780', 88000, '2023-07-28'),
+(62, 'Barang Keluar', 1, 0, 'INV-BM-2023072816334163510', 57000, '2023-07-28'),
+(63, 'Barang Keluar', 1, 0, 'INV-BM-2023072816342589983', 85000, '2023-07-28'),
+(64, 'Barang Keluar', 1, 0, 'INV-BM-2023072816345343317', 180000, '2023-07-28'),
+(65, 'Barang Keluar', 1, 0, 'INV-BM-2023072816354356568', 159000, '2023-07-28'),
+(66, 'Barang Keluar', 1, 0, 'INV-BM-2023072816365029369', 242000, '2023-07-28'),
+(67, 'Barang Keluar', 1, 0, 'INV-BM-2023072816375138569', 81000, '2023-07-28'),
+(68, 'Barang Keluar', 1, 0, 'INV-BM-2023072816383396433', 84000, '2023-07-28'),
+(69, 'Barang Keluar', 1, 0, 'INV-BM-2023072816390824439', 145000, '2023-07-28'),
+(70, 'Barang Keluar', 1, 0, 'INV-BM-2023072816403064520', 116000, '2023-07-28'),
+(71, 'Barang Keluar', 1, 0, 'INV-BM-2023072816414686802', 121000, '2023-07-28'),
+(72, 'Barang Keluar', 1, 0, 'INV-BM-2023072816422184537', 39000, '2023-07-28'),
+(73, 'Barang Keluar', 1, 0, 'INV-BM-2023072816424663315', 26000, '2023-07-28'),
+(74, 'Barang Keluar', 1, 0, 'INV-BM-2023072816431007124', 72000, '2023-07-28');
 
 -- --------------------------------------------------------
 
@@ -244,13 +285,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=144;
 
 --
 -- AUTO_INCREMENT for table `entitas`
@@ -268,7 +309,7 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 
 --
 -- AUTO_INCREMENT for table `users`
